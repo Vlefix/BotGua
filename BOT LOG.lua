e74bf55c6d3e992ffae093dd56a1994a61fd3ea7
Vlefix <116861762+Vlefix@users.noreply.github.com>
2022-10-28 11:16:34 +0700

    Add files via upload

---
+++ BOT LOG.lua
@@ -0,0 +1,69 @@
+Rdp = {
+    {nordp = 1, botname = "Bot1", Status = "Nonaktif"},
+    {nordp = 1, botname = "Bot1", Status = "Nonaktif"},
+	{nordp = 1, botname = "Bot1", Status = "Nonaktif"},
+    {nordp = 2, botname = "Bot2", Status = "Nonaktif"},
+    {nordp = 2, botname = "Bot2", Status = "Nonaktif"},
+	{nordp = 2, botname = "Bot2", Status = "Nonaktif"},
+    {nordp = 3, botname = "Bot3", Status = "Nonaktif"},
+	{nordp = 3, botname = "Bot3", Status = "Nonaktif"},
+    {nordp = 3, botname = "Bot3", Status = "Nonaktif"},
+    {nordp = 4, botname = "Bot4", Status = "Nonaktif"},
+	{nordp = 4, botname = "Bot4", Status = "Nonaktif"},
+    {nordp = 4, botname = "Bot4", Status = "Nonaktif"},
+    {nordp = 5, botname = "Bot5", Status = "Nonaktif"},
+	{nordp = 5, botname = "Bot5", Status = "Nonaktif"},
+    {nordp = 5, botname = "Bot5", Status = "Nonaktif"},
+    {nordp = 6, botname = "Bot6", Status = "Nonaktif"},
+    {nordp = 6, botname = "Bot6", Status = "Nonaktif"},
+	{nordp = 6, botname = "Bot6", Status = "Nonaktif"},
+	{nordp = 7, botname = "Bot7", Status = "Nonaktif"},
+	{nordp = 7, botname = "Bot7", Status = "Nonaktif"},
+	{nordp = 7, botname = "Bot7", Status = "Nonaktif"},
+	{nordp = 8, botname = "Bot8", Status = "Nonaktif"},
+	{nordp = 8, botname = "Bot8", Status = "Nonaktif"},
+	{nordp = 8, botname = "Bot8", Status = "Nonaktif"},
+	{nordp = 9, botname = "Bot9", Status = "Nonaktif"},
+	{nordp = 9, botname = "Bot9", Status = "Nonaktif"},
+	{nordp = 9, botname = "Bot9", Status = "Nonaktif"},
+	{nordp = 10, botname = "Bot10", Status = "Nonaktif"},
+	{nordp = 10, botname = "Bot10", Status = "Nonaktif"},
+	{nordp = 10, botname = "Bot10", Status = "Nonaktif"},
+	{nordp = 11, botname = "Bot11", Status = "Nonaktif"},
+	{nordp = 11, botname = "Bot11", Status = "Nonaktif"},
+	{nordp = 11, botname = "Bot11", Status = "Nonaktif"},
+	{nordp = 12, botname = "Bot12", Status = "Nonaktif"},
+	{nordp = 12, botname = "Bot12", Status = "Nonaktif"},
+	{nordp = 12, botname = "Bot12", Status = "Nonaktif"},
+	{nordp = 13, botname = "Bot13", Status = "Nonaktif"},
+	{nordp = 13, botname = "Bot13", Status = "Nonaktif"},
+	{nordp = 13, botname = "Bot13", Status = "Nonaktif"},
+	
+}
+
+ShowIP = true
+ip_add = {
+    "216.238.75.169",
+    "104.248.229.66",
+    "157.245.199.246",
+    "134.209.191.213",
+    "167.172.84.239",
+    "20.235.241.34",
+    "4.240.112.198",
+	"134.209.177.58",
+	"178.128.60.9",
+	"188.166.254.25",
+	"5.252.97.212",
+	"165.232.172.113",
+	"167.172.84.174",
+}
+
+hookURL = "https://discord.com/api/webhooks/1032179092189085696/hdTP7xpe9guhKdpzdKjAZRXCAPNP1DQuu2xahH1eJt2R87Yo_3VFaEAmz5ArpuoyH9N-"
+
+Teks_Deskripsi = "List Nick Para Babu"
+Teks_Footer = "Golek Duit Kanggo Masa Depan"
+
+Emote_Offline = "<:offline:982957367392088124>"
+Emote_Online = "<:online:982957403756716052>"
+
+menit = 5 --TIAP BERAPA MENIT BOTNYA NGIRIM WEBHOOK
